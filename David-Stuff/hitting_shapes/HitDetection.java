c0e7ec0c4adb27e542dc74da9603623d75c9dbb7
Davidd <dediudavidyt@gmail.com>
2024-09-04 23:28:45 +0200

    started working on the assignment

---
+++ David-Stuff/hitting_shapes/HitDetection.java
@@ -0,0 +1,26 @@
+/**
+ * Detects if a point hits any of two circles.
+ * 
+ * Usage:
+ * TODO: Documentation
+ * 
+ * END TODO
+ * 
+ * @author <NAME STUDENT 1>
+ * @ID <ID STUDENT 1>
+ * @author <NAME STUDENT 2>
+ * @ID <ID STUDENT 2>
+ * 
+ */
+class HitDetection {
+    
+    public void run(){
+        // TODO: Hit detection
+        
+        // END TODO
+    }
+
+    public static void main(String[] args) {
+        new HitDetection().run();
+    }
+}
