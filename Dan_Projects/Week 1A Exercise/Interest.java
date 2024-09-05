364d10969e515cc162c8cd6b3d7ebaf4ce16cd2c
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-05 10:25:37 +0200

    full_Interest

--- Dan_Projects/Week 1A Exercise/Interest.java
+++ Dan_Projects/Week 1A Exercise/Interest.java
@@ -6,14 +6,27 @@ public class Interest{
     public void calculateInterest() {
         double balance; /**in Euro */
         double rate;
+        double tax;
 
         System.out.print("How much money you got: ");
         balance = scanner.nextDouble();
 
         System.out.print("Give interest rate as a percentage: ");
         rate = scanner.nextDouble();
-        balance = (balance + (balance * rate / 100));
+
+        System.out.print("How much is the tax as a percentage: ");
+        tax = scanner.nextDouble();
+
+
+        balance = (balance + (balance * rate / 100));  
+         /**I chose to calculate the new balance before the print statement, since it 
+         makes the code cleaner and easier to handle in the future modifications of 
+        the code. Had the objective of this program would have been something that 
+        required different versions of the balance depending on the choice, not 
+        relying only on interest, I would have calculated it in the print statement */
         System.out.println("After a year, your balance is EUR " + balance);
+        System.out.println("You'll have to pay in taxes: " + balance * tax /100);
+
     }
 
     public static void main(String[] args){
