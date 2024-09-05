42736ac163e48d0ca178144cb54864cc22cad297
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-05 13:31:08 +0200

    HitDetectionBaseDocuments

---
+++ Dan_Projects/Assignments/Assignment_1 Hit Detection/hitting_shapes/HitDetection.java
@@ -0,0 +1,26 @@
+/**
+ * Detects if a point hits any of two circles.
+ * 
+ * Usage:
+ * TODO: Documentation
+ * 
+ * END TODO
+ * 
+ * @author < Dediu David Gabriel>
+ * @ID <ID STUDENT 1>
+ * @author < Vasilescu Dan Gabriel>
+ * @ID <2155699>
+ * 
+ */
+class HitDetection {
+    
+    public void run() {
+        // TODO: Hit detection
+
+        // END TODO
+    }
+
+    public static void main(String[] args) {
+        new HitDetection().run();
+    }
+}
