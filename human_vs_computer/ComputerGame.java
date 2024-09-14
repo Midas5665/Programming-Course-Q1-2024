b4b6ef7a673afe9fe053d770d0b9174b3554279c
David-Gabriel Dediu <135978285+Davidd0605@users.noreply.github.com>
2024-09-14 16:43:36 +0200

    Added my ver of part 2, to be edited

--- human_vs_computer/ComputerGame.java
+++ human_vs_computer/ComputerGame.java
@@ -10,72 +10,67 @@ import java.util.*;
  * Ok so computer game. The basic plan is to use scanner to 
  * make the computer modify the random pool where it will take numbers from.
  * 
- * @author TODO
- * @id     TODO
- * @author TODO
- * @id     TODO
- * @data   TODO
+ * @author Dediu David Gabriel
+ * @ID <2096676>
+ * @author Vasilescu Dan Gabriel
+ * @ID <2155699> 
+ * @data  14/09/2024
  */
 public class ComputerGame {
     Scanner scanner = new Scanner(System.in);
     Random rand = new Random();
+
     void run() {
         // TODO: Implementation
         System.out.println("Think of a secret number between not smaller than 0 and not greater than 999. Type 'go' when you have one.");
-        gameStart();
-        //if(scanner.next().equals("go")){
-        //    gameStart(); //Debugging. TO BE REMOVED
-        //}
+
         // END TODO
     }
     //Random borders: rand = rand.nextInt(topBorder-bottomBorder + 1) + bottomBorder
-    public void gameStart(){
-        int numberOfGuesses = 10;
-        int bottomBorder=0;
-        int topBorder=1000;
-        String response;
 
+    public void gameStart(){
+
+        int numberOfGuesses = 10;
+        int bottomBorder = 0;
+        int topBorder = 1000;
+        String response = "";
+        Boolean hasGuessed = false;
         //int guessedNumber = rand.nextInt(bottomBorder, topBorder);
         //System.out.println(guessedNumber);
         //numberOfGuesses--;
         
         //scanner.hasNext();
-        if(scanner.next().equals("go")){
-            //gameStart(); //Debugging. TO BE REMOVED
-        
-            while(numberOfGuesses != 0){
-                int guessedNumber = rand.nextInt(bottomBorder, topBorder);
-                System.out.println(guessedNumber);
-                System.out.println("Top border: " + topBorder);
-                System.out.println("Bottom border: " + bottomBorder);
-                numberOfGuesses--;
-                response = scanner.next();
-            
-                if(response.equals("lower")){
-                    if(guessedNumber==topBorder){
-                        topBorder-=1;
-                    }else{
-                    topBorder = guessedNumber;
-                    }
-                    
-                } else if(response.equals("higher")){
-                    if(guessedNumber==bottomBorder){
-                        bottomBorder+=1;
-                    }else{
-                    bottomBorder = guessedNumber;
-                    
-                    }
-                
-                } else if(response.equals("guessed")){
-                    scanner.close();
-                    numberOfGuesses=0;
-                }
-            
-                if(numberOfGuesses == 0){
-                    System.out.println("I give up");
-                }
 
+        if (scanner.next().equals("go")) {
+
+            while (numberOfGuesses != 0 && !hasGuessed) {
+                int middle = (topBorder + bottomBorder) / 2;
+                numberOfGuesses--;
+                System.out.println(middle);
+                response = scanner.next();
+
+                switch (response) {
+                    case "higher": {
+                        bottomBorder = middle;
+                    } 
+                        break;
+                    case "lower": {
+                        topBorder = middle;
+                    }
+                        break;
+                    case "guessed": {
+                        hasGuessed = true;
+                    }
+                        break;
+                    default:
+
+
+                }
             }
+            if (numberOfGuesses == 0 && !response.equals("guessed")) {
+                System.err.println("I give up");
+            }
+
         }
     }
 
--- human_vs_computer/HumanGame.java
+++ human_vs_computer/HumanGame.java
@@ -7,11 +7,11 @@ import java.util.*; // For Scanner, Random, etc.
  * 99 that you have to guess in at most seven tries. Afterwards, you see your
  * guessing history so you can learn to better play the game.
  * 
- * @author TODO
- * @id     TODO
- * @author TODO
- * @id     TODO
- * @data   TODO
+ * @author Dediu David Gabriel
+ * @ID <2096676>
+ * @author Vasilescu Dan Gabriel
+ * @ID <2155699> 
+ * @data  14/09/2024
  */
 public class HumanGame {
     Scanner sc = new Scanner(System.in);
@@ -42,7 +42,12 @@ public class HumanGame {
         if (userInput > n) {
             System.out.println("lower");
         }
+        if (userInput == n) {
+            System.out.println("Good guess! You won.");
+        }
+
         while (userInput != n && numberOfGuesses > 0 && sc.hasNextInt()) {
+            
             userInput = sc.nextInt();
             numberOfGuesses--;
             guesses.add(userInput);
@@ -60,8 +65,9 @@ public class HumanGame {
             }
         }
         sc.close(); 
-        System.out.println((7-numberOfGuesses) + " guess" 
-        + ( 7-numberOfGuesses == 1 ? " :" : "es :"));
+
+        System.out.println((7 - numberOfGuesses) 
+        + " guess" + (7 - numberOfGuesses == 1 ? " :" : "es :"));
 
         for (int i : guesses) {
             for (int j = 0; j < 99; j++) {
