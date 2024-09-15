9a1c0cd9752de9c1053129c11885d5aa2955a6f5
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-15 11:43:49 +0200

    KYSS

--- human_vs_computer/HumanGame.java
+++ human_vs_computer/HumanGame.java
@@ -7,11 +7,14 @@ import java.util.*; // For Scanner, Random, etc.
  * 99 that you have to guess in at most seven tries. Afterwards, you see your
  * guessing history so you can learn to better play the game.
  * 
+<<<<<<< HEAD
  * @author Vasilescu Dan Gabriel
  * @id     2155699
  * @author TODO
  * @id     TODO
  * @data   TODO
+=======
+>>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
  * @author Dediu David Gabriel
  * @ID <2096676>
  * @author Vasilescu Dan Gabriel
@@ -69,8 +72,11 @@ public class HumanGame {
             }
         }
         sc.close(); 
+<<<<<<< HEAD
         System.out.println((7 - numberOfGuesses) + " guess" 
             + (7 - numberOfGuesses == 1 ? " :" : "es :"));
+=======
+>>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
 
         System.out.println((7 - numberOfGuesses) 
         + " guess" + (7 - numberOfGuesses == 1 ? " :" : "es :"));
