b88dd85324f0c6d5d7dd0cd7a69682991b59f889
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-15 16:33:22 +0200

    okayNow?

--- human_vs_computer/HumanGame.java
+++ human_vs_computer/HumanGame.java
@@ -7,17 +7,6 @@ import java.util.*; // For Scanner, Random, etc.
  * 99 that you have to guess in at most seven tries. Afterwards, you see your
  * guessing history so you can learn to better play the game.
  * 
-<<<<<<< HEAD
-<<<<<<< HEAD
- * @author Vasilescu Dan Gabriel
- * @id     2155699
- * @author TODO
- * @id     TODO
- * @data   TODO
-=======
->>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
-=======
->>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
  * @author Dediu David Gabriel
  * @ID <2096676>
  * @author Vasilescu Dan Gabriel
@@ -75,17 +64,9 @@ public class HumanGame {
             }
         }
         sc.close(); 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println((7 - numberOfGuesses) + " guess" 
-            + (7 - numberOfGuesses == 1 ? " :" : "es :"));
-=======
->>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
-=======
->>>>>>> 7397b8ff1e53f25d376d3a2616ca42fe5fde6297
 
         System.out.println((7 - numberOfGuesses) 
-        + " guess" + (7 - numberOfGuesses == 1 ? " :" : "es :"));
+            + " guess" + (7 - numberOfGuesses == 1 ? " :" : "es :"));
 
         for (int i : guesses) {
             for (int j = 0; j < 99; j++) {
