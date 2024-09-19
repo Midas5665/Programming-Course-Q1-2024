f45834adc03e6e2fcbe0efd5ebe1872b993aa11d
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-19 09:37:21 +0200

    FunWithFunctionsCompletedPartially

--- Dan_Projects/Week 3A Exercise/FunWithFunctions/FunWithFunctions.java
+++ Dan_Projects/Week 3A Exercise/FunWithFunctions/FunWithFunctions.java
@@ -6,10 +6,11 @@ public class FunWithFunctions {
     }
 
     public static void printTable(){
-        System.out.printf("   x    f(x) %n");
+        System.out.printf("   x     f(x) %n");
         System.out.printf("---------------%n");
         for(double i=-3;i<=3;i+=0.5){
-            System.out.printf("%0+-4.1f %1+-4.1f %n", i, f(i));
+            System.out.printf("%5.2f   %5.2f %n", i, f(i));
+            
         }
     }
     public static void main(String[] args) {
