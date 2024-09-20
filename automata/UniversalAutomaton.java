43ee54a64215ff6c14fb321e125f17d355aec707
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-20 18:17:34 +0200

    yoAngelo

--- automata/UniversalAutomaton.java
+++ automata/UniversalAutomaton.java
@@ -14,23 +14,83 @@ class UniversalAutomaton {
     Scanner scanner = new Scanner(System.in);
 
     String genToString(boolean[] gen) {
-        // TODO 13: Copy from ABAutomaton.java
-        return "Hello";
+        // TODO 5
+        String resultString = "";
+        for (int i = 1; i < gen.length; i++) {
+            if (gen[i]) {
+                resultString += "*";
+            }
+            else {
+                resultString += " ";
+            }
+        }
+        return resultString;
     }
 
     boolean[] nextGen(boolean[] ruleSequence, boolean[] gen) {
         // TODO 14
-        return new boolean[] { true, false };
+        boolean[] resultArray = new boolean[gen.length];
+        resultArray[0] = resultArray[resultArray.length - 1] = false;
+        for (int i = 1; i < gen.length - 1; i++) {
+            resultArray[i] = false;
+            if (ruleSequence[0] && !(gen[i - 1] || gen[i] || gen[i + 1])) { 
+                resultArray[i] = true;
+            }
+            if (ruleSequence[1] && (!gen[i - 1] && !gen[i] && gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[2] && (!gen[i - 1] && gen[i] && !gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[3] && (!gen[i - 1] && gen[i] && gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[4] && (gen[i - 1] && !gen[i] && !gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[5] && (gen[i - 1] && !gen[i] && gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[6] && (gen[i - 1] && gen[i] && !gen[i + 1])) {
+                resultArray[i] = true;
+            }
+            if (ruleSequence[7] && (gen[i - 1] && gen[i] && gen[i + 1])) {
+                resultArray[i] = true;
+            }
+        }
+        return resultArray;
     }
 
     boolean[] readInitalGeneration(int length) {
-        // TODO 16: Copyt from ABAutomaton.java
-        return new boolean[] { true, false };
+        // TODO 11
+        boolean[] resultArray = new boolean[length + 2];
+        resultArray[0] = resultArray[length + 1] = false;
+        for (int i = 1; i <= length; i++) {
+            resultArray[i] = false;
+        }
+        if (scanner.next().equals("init_start")) {
+            String input = scanner.next();
+            while (!input.equals("init_end")) {
+                resultArray[Integer.valueOf(input)] = true;
+                input = scanner.next();
+            }
+        }
+        return resultArray;
     }
 
     boolean[] readRuleSequence() {
         // TODO 17
-        return new boolean[] { true, false };
+        boolean[] ruleArray = new boolean[8];
+        for (int i = 0; i < 8; i++) {
+            int arrayInput = scanner.nextInt();
+            if (arrayInput == 0) {
+                ruleArray[i] = false;
+            }
+            if (arrayInput == 1) {
+                ruleArray[i] = true;
+            }
+        }
+        return ruleArray;
     }
 
     void run() {
