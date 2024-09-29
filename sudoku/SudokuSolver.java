405c1e8a01226ffbdbcc8dccc54ec1313f7c380e
Vasilescu <d.g.vasilescu@student.tue.nl>
2024-09-29 12:02:55 +0200

    newNewAttempt

--- sudoku/SudokuSolver.java
+++ sudoku/SudokuSolver.java
@@ -1,11 +1,9 @@
 import java.awt.Point;
-
 public class SudokuSolver {
     SudokuGrid grid;
     SudokuGrid lastSolution = null;
     SudokuGrid firstSolution = null;
     static int numberOfSolutions;
-
     public SudokuSolver(SudokuGrid grid) {
         // Initialize the SudokuSolver with the provided SudokuGrid
         this.grid = grid;
@@ -13,44 +11,39 @@ public class SudokuSolver {
     
     public boolean solve() {
         // Use a recursive strategy to solve the Sudoku puzzle
-        //solution condition
-        Point auxPoint = grid.findEmptyCell();
-        if (auxPoint == null) {
+        Point nextEmptyPoint = grid.findEmptyCell();
+        SudokuGrid oldGrid = grid.copy();
+        if(nextEmptyPoint == null)
+        {
+            numberOfSolutions ++;
+            if(firstSolution == null)
+                firstSolution = grid.copy();
+            lastSolution = grid.copy();
             return true;
         }
-
-        for (int i = 1; i <= 9; i++) {
-            int x = i;
-            //find next empty cell
-            SudokuGrid oldGrid = grid.copy();
-            if (!grid.givesConflict(auxPoint.x, auxPoint.y, x)) {
-                grid.fillCell(auxPoint.x, auxPoint.y, x);
-                boolean isSolution = solve();
-                if (isSolution) {
-                    return true;
+        for(int i = 1; i <= 9; i ++) {
+            
+            if(!grid.givesConflict(nextEmptyPoint.x, nextEmptyPoint.y, i)) {
+                grid.fillCell(nextEmptyPoint.x, nextEmptyPoint.y, i);
+                if(solve() == false) {
+                    grid = oldGrid.copy();
                 }
-                else {
-                    grid = oldGrid;
-                }
+                else
+                    i = 1;
             }
         }
         return false;
     }
     
     public void solveIt() {
-        // Use solve() to solve the puzzle and print the solution or
-        // a message if no solution is found
-        boolean isSolution = solve();
-        if (isSolution) {
-            grid.print();
-        }
-        else {
-            System.out.println("No solution");
-        }
+         // Use solve() to solve the puzzle and print the solution or a message if no solution is found
+         boolean rez = solve();
+         firstSolution.print();
+         lastSolution.print();
+         System.out.println(rez);
     }
 
     public static void main(String[] args) {
-
         // Create a SudokuGrid and a SudokuSolver
         SudokuGrid grid = new SudokuGrid();
         SudokuSolver solver = new SudokuSolver(grid);
